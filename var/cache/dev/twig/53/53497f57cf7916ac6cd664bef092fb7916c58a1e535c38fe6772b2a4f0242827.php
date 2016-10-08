af5d630c66715d74010332036803b420eb1f00b4
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-10-08 00:57:07 -0300

    Initial commit

---
+++ var/cache/dev/twig/53/53497f57cf7916ac6cd664bef092fb7916c58a1e535c38fe6772b2a4f0242827.php
@@ -0,0 +1,48 @@
+<?php
+
+/* @Security/Collector/icon.svg */
+class __TwigTemplate_0862139289c7dcca6157fbe90a973ac5777536a007d28f9482c0b2154d2ba720 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_e07ced89f6fe6ac8d84efe228d382e4e738d0f074fd71193d1258400624a7d1d = $this->env->getExtension("native_profiler");
+        $__internal_e07ced89f6fe6ac8d84efe228d382e4e738d0f074fd71193d1258400624a7d1d->enter($__internal_e07ced89f6fe6ac8d84efe228d382e4e738d0f074fd71193d1258400624a7d1d_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@Security/Collector/icon.svg"));
+
+        // line 1
+        echo "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" enable-background=\"new 0 0 24 24\" xml:space=\"preserve\">
+    <path fill=\"#AAAAAA\" d=\"M21,20.4V22H3v-1.6c0-3.7,2.4-6.9,5.8-8c-1.7-1.1-2.9-3-2.9-5.2c0-3.4,2.7-6.1,6.1-6.1s6.1,2.7,6.1,6.1c0,2.2-1.2,4.1-2.9,5.2C18.6,13.5,21,16.7,21,20.4z\"/>
+</svg>
+";
+        
+        $__internal_e07ced89f6fe6ac8d84efe228d382e4e738d0f074fd71193d1258400624a7d1d->leave($__internal_e07ced89f6fe6ac8d84efe228d382e4e738d0f074fd71193d1258400624a7d1d_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Security/Collector/icon.svg";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" enable-background=\"new 0 0 24 24\" xml:space=\"preserve\">
+    <path fill=\"#AAAAAA\" d=\"M21,20.4V22H3v-1.6c0-3.7,2.4-6.9,5.8-8c-1.7-1.1-2.9-3-2.9-5.2c0-3.4,2.7-6.1,6.1-6.1s6.1,2.7,6.1,6.1c0,2.2-1.2,4.1-2.9,5.2C18.6,13.5,21,16.7,21,20.4z\"/>
+</svg>
+";
+    }
+}
---
+++ var/cache/dev/twig/dc/dc6864547c7bc2dc76f8d8f4e3a5a0ab485d524035970e0675b1ddb27f183bb3.php
@@ -0,0 +1,44 @@
+<?php
+
+/* UserBundle:Default:index.html.twig */
+class __TwigTemplate_41172045184fee81287cf86cb7ae1540fb90b063fccdadeb2119e8d8dc5db5a3 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_8af1bb2ebc410da02fa41354c899b8eac47b145c5fe4085e6b55227f45a8ae86 = $this->env->getExtension("native_profiler");
+        $__internal_8af1bb2ebc410da02fa41354c899b8eac47b145c5fe4085e6b55227f45a8ae86->enter($__internal_8af1bb2ebc410da02fa41354c899b8eac47b145c5fe4085e6b55227f45a8ae86_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "UserBundle:Default:index.html.twig"));
+
+        // line 1
+        echo "Hello World!
+";
+        
+        $__internal_8af1bb2ebc410da02fa41354c899b8eac47b145c5fe4085e6b55227f45a8ae86->leave($__internal_8af1bb2ebc410da02fa41354c899b8eac47b145c5fe4085e6b55227f45a8ae86_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "UserBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "Hello World!
+";
+    }
+}
