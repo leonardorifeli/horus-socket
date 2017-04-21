7e548f1d1dcfb102ad92196e09dbf9b87a2b34a3
Leonardo Rifeli <leonardorifeli@gmail.com>
2017-04-21 00:37:20 -0300

    Replace space to tabs

--- src/SocketServerBundle/Business/Service/SocketServerService.php
+++ src/SocketServerBundle/Business/Service/SocketServerService.php
@@ -8,77 +8,77 @@ use Ratchet\ConnectionInterface;
 class SocketServerService implements MessageComponentInterface
 {
 
-    public static $connections;
+	public static $connections;
 
-    public function __construct()
-    {
-        self::$connections = new \SplObjectStorage;
-    }
+	public function __construct()
+	{
+		self::$connections = new \SplObjectStorage;
+	}
 
-    public function onOpen(ConnectionInterface $connection)
-    {
-        $queryExplode = explode('&', $connection->WebSocket->request->getQuery());
-        $queryParams = [];
+	public function onOpen(ConnectionInterface $connection)
+	{
+		$queryExplode = explode('&', $connection->WebSocket->request->getQuery());
+		$queryParams = [];
 
-        foreach ($queryExplode as $queryParam) {
-            $queryParamExplode = explode('=', $queryParam);
-            $queryParamKey = $queryParamExplode[0];
-            array_shift($queryParamExplode);
-            $queryParamValue = implode($queryParamExplode);
-            $queryParams[$queryParamKey] = $queryParamValue;
-        }
+		foreach ($queryExplode as $queryParam) {
+			$queryParamExplode = explode('=', $queryParam);
+			$queryParamKey = $queryParamExplode[0];
+			array_shift($queryParamExplode);
+			$queryParamValue = implode($queryParamExplode);
+			$queryParams[$queryParamKey] = $queryParamValue;
+		}
 
-        if (!array_key_exists('name', $queryParams)) {
-            $connection->close();
-            return;
-        }
+		if (!array_key_exists('name', $queryParams)) {
+			$connection->close();
+			return;
+		}
 
-        if (!array_key_exists('room', $queryParams)) {
-            $connection->close();
-            return;
-        }
+		if (!array_key_exists('room', $queryParams)) {
+			$connection->close();
+			return;
+		}
 
-        $session = new \stdClass;
-        $session->room = $queryParams['room'];
-        $session->name = $queryParams['name'];
-        $connection->session = $session;
+		$session = new \stdClass;
+		$session->room = $queryParams['room'];
+		$session->name = $queryParams['name'];
+		$connection->session = $session;
 
-        self::$connections->attach($connection);
-    }
+		self::$connections->attach($connection);
+	}
 
-    public function onMessage(ConnectionInterface $connection, $jsonMessage)
-    {
-        foreach (self::$connections as $otherConnection) {
-            if($otherConnection->session->room != $connection->session->room)
-                continue;
+	public function onMessage(ConnectionInterface $connection, $jsonMessage)
+	{
+		foreach (self::$connections as $otherConnection) {
+			if($otherConnection->session->room != $connection->session->room)
+				continue;
 
-            if ($connection !== $otherConnection) {
-                $otherConnection->send($jsonMessage);
-            }
-        }
-    }
+			if ($connection !== $otherConnection) {
+				$otherConnection->send($jsonMessage);
+			}
+		}
+	}
 
-    public function onClose(ConnectionInterface $connection)
-    {
-        self::$connections->detach($connection);
-    }
+	public function onClose(ConnectionInterface $connection)
+	{
+		self::$connections->detach($connection);
+	}
 
-    public function onError(ConnectionInterface $connection, \Exception $e)
-    {
-        $connection->close();
-    }
+	public function onError(ConnectionInterface $connection, \Exception $e)
+	{
+		$connection->close();
+	}
 
-    public function sendBotMessage()
-    {
-        foreach (self::$connections as $connection) {
-            $message = [
-                "user" => "Server",
-                "room" => "meetupPHPSC",
-                "text" => "Hello users! I'm a boot.",
-                "time" => date("H:i a"),
-            ];
+	public function sendBotMessage()
+	{
+		foreach (self::$connections as $connection) {
+			$message = [
+				"user" => "Server",
+				"room" => "meetupPHPSC",
+				"text" => "Hello users! I'm a boot.",
+				"time" => date("H:i a"),
+			];
 
-            $connection->send(json_encode($message));
-        }
-    }
+			$connection->send(json_encode($message));
+		}
+	}
 }
